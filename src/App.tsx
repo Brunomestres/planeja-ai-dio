b1677371aa883a00ab1dfa62661d1c790b37b00a
Bruno <bruno.mestres@poscontrole.com.br>
2026-06-06 14:57:18 -0300

    feat: add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
 function App() {
-  return <p>Hello world</p>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
